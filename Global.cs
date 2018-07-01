a181c94c65a367804e9606deb684d2832a965669
Ethan Slagle <35624713+ETSlagle@users.noreply.github.com>
2018-06-30 21:26:42 -0400

    Add files via upload

---
+++ Global.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Scripts
+{
+    public static class Global
+    {
+        public static string current_color;
+        public static float minspeed = 10f;
+        public static float maxspeed = 20f;
+       
+        public static float PointsperSec = 1;
+
+    }
+}
---
+++ Points_By_Color.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Points_By_Color : MonoBehaviour {
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (gameObject.tag == Assets.Scripts.Global.current_color)
+        {
+            Assets.Scripts.Global.PointsperSec += 19;
+
+        }
+    }
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        if (gameObject.tag == Assets.Scripts.Global.current_color)
+            Assets.Scripts.Global.PointsperSec -= 19;
+    }
+}
+	
+
